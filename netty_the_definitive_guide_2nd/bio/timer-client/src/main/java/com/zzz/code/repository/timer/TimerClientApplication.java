86bca6da5e0b45999a0d6811981ded77e388ac55
zhaowei.zhang <zhaowei.zhang@jia007.com>
2018-11-23 10:23:41 +0800

    提交客户端初版的代码.

---
+++ netty_the_definitive_guide_2nd/bio/timer-client/src/main/java/com/zzz/code/repository/timer/TimerClientApplication.java
@@ -0,0 +1,13 @@
+package com.zzz.code.repository.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimerClientApplication.class, args);
+    }
+
+}
